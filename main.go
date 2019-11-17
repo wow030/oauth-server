0e2de9ec6c4f4a57e6767ac4ec64101febe2f74a
wow030 <zaq05206@gmail.com>
2019-11-17 23:06:10 +0800

    disable https

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	server := service.CreateService()
 
-	err := http.ListenAndServeTLS(":8081", "server.crt", "server.key", server.Mux)
+	err := http.ListenAndServe(":81", server.Mux)
 	if err != nil {
 		log.Fatal(err)
 	}
